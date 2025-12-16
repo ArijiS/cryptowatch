5a6ce149ae589e812873c08b6e16917d6a466e9b
Arijit Sinha <arijitsinha2.0@gmail.com>
2025-12-16 15:22:06 +0530

    graph styled, minor coin component styling changes

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -5,6 +5,7 @@ import { marketCapConversion } from '../utils/marketCapConversion';
 import LineChart from './LineChart';
 import Loader from "./Loader";
 import { RxExternalLink } from "react-icons/rx";
+import { BiLinkExternal } from "react-icons/bi";
 
 import { FaRegThumbsUp } from "react-icons/fa";
 import { FaRegThumbsDown } from "react-icons/fa";
@@ -71,87 +72,112 @@ const Coin = () => {
     <section className="max-w-360 px-25 py-10 mx-auto">
       {
         (loading || !coinData || !historicalData) ? <Loader /> : (
-          <div className="max-w-4xl mx-auto my-16 flex flex-col gap-y-6 items-center justify-center">
-            <div className="relative flex flex-col items-center justify-center gap-y-4 bg-primary-100 w-full rounded-2xl p-6">
-              <div className="absolute top-0 left-0 right-0 h-0.5 bg-linear-to-r from-transparent via-accent-50 to-transparent opacity-55"/>
+          <div className="max-w-5xl mx-auto my-16 flex flex-col gap-y-6 items-center justify-center">
+            <div className="relative flex flex-col items-center justify-center gap-y-4 bg-primary-100/40 w-full rounded-2xl p-6">
+              <div className="absolute top-0 left-0 right-0 h-0.5 bg-linear-to-r from-transparent via-accent-50 to-transparent opacity-30"/>
               <img src={coinData.image?.large} alt={coinData.name} className="size-15"/>
               <h1 className="text-4xl font-medium tracking-tight">{`${coinData.name} - (${coinData.symbol})`}</h1>
               <a href={coinData.links?.homepage[0]} className="flex items-center gap-x-2 text-lg font-light tracking-tight" target="_blank">{`${coinData.name}-Official Website`}<RxExternalLink className="size-5" strokeWidth= {0.5}/>
               </a>
             </div>
 
-            <div className= "bg-primary-100 w-full p-6 rounded-2xl"> {/* chart component*/}
+            <div className= "bg-primary-100/40 w-full p-6 rounded-2xl"> {/* chart component*/}
               < LineChart historicalData = {historicalData}/>
             </div>
 
             <div className= "grid grid-cols-6 grid-rows-5 gap-5 w-full [&>*]:rounded-2xl [&>*]:p-4"> {/* Info grid layout */}
 
-              <div className="bg-primary-100 col-span-2 row-span-2 flex flex-col justify-between">
+              <div className="bg-primary-100/40 col-span-2 row-span-2 flex flex-col justify-between">
                 <p className="w-fit text-2xl text-left font-normal tracking-wide px-4 py-1 border-accent-50/60 border-2 rounded-xl">Market Rank</p>
                 <p className={`${coinData.market_cap_rank < 100 ? "text-9xl/27 font-medium" : "text-6xl tracking-wide"}`}>{coinData.market_cap_rank}</p>
               </div>
 
-              <div className="bg-primary-100 col-span-2 row-span-2 flex flex-col justify-between">
+              <div className="bg-primary-100/40 col-span-2 row-span-2 flex flex-col justify-between">
                 <p className="w-fit text-2xl font-normal tracking-wide px-4 py-1 border-accent-50/60 border-2 rounded-xl">Current Price</p>
-                <p className={`${coinData.market_data.current_price[currency.name].toString().length > 7 ? "text-3xl font-medium" : "text-6xl font-medium"}`}>
+                <p className={`${coinData.market_data.current_price[currency.name].toString().length > 6 ? "text-4xl tracking-wide font-medium" : "text-6xl tracking-tight"}`}>
                   {`${currency.symbol} ${coinData.market_data.current_price[currency.name]}`}</p>
               </div>
               
-              <div className="bg-primary-100 col-span-2 row-span-2 flex flex-col justify-between">
+              <div className="bg-primary-100/40 col-span-2 row-span-2 flex flex-col justify-between">
                 <p className="w-fit text-2xl tracking-wide px-4 py-1 border-accent-50/60 border-2 rounded-xl">Market Cap</p>
-                <p className="text-5xl font-medium">{`${currency.symbol} ${marketCapConversion(coinData.market_data.market_cap[currency.name])}`}</p>
+                <p className="text-[2.75rem]/[2.75rem] font-medium">{`${currency.symbol} ${marketCapConversion(coinData.market_data.market_cap[currency.name])}`}</p>
               </div>
 
-              <div className="bg-primary-100 col-span-2 row-span-2 flex flex-col justify-between">
+              <div className="bg-primary-100/40 col-span-2 row-span-2 flex flex-col justify-between">
                 <p className="w-fit text-2xl tracking-wide px-4 py-1 border-accent-50/60 border-2 rounded-xl">24H Movement</p>
-                <p className={`${coinData.market_data.price_change_percentage_24h < 1 ? "text-red-400" : "text-green-400"} text-6xl font-medium`}>
+                <p className={`${coinData.market_data.price_change_percentage_24h < 1 ? "text-red-400" : "text-green-400"} text-5xl font-medium tracking-tighter`}>
                   {`${coinData.market_data.price_change_percentage_24h.toFixed(2)} %`}</p>
               </div>
 
-              <div className="bg-primary-100 col-span-2 row-span-2 flex flex-col justify-between">
+              <div className="bg-primary-100/40 col-span-2 row-span-2 flex flex-col justify-between">
                 <p className="w-fit text-2xl tracking-wide px-4 py-1 border-accent-50/60 border-2 rounded-xl">1Y Return</p>
-                <p className={`${coinData.market_data.price_change_percentage_1y < 1 ? "text-red-400" : "text-green-400"} text-6xl font-medium`}>
+                <p className={`${coinData.market_data.price_change_percentage_1y < 1 ? "text-red-400" : "text-green-400"} text-5xl font-medium tracking-tighter`}>
                   {`${coinData.market_data.price_change_percentage_1y.toFixed(2)} %`}</p>
               </div>
 
               <div className="col-span-2 row-span-2 flex flex-col gap-y-4 [&>*]:p-4 [&>*]:rounded-2xl [&>*]:flex [&>*]:flex-col [&>*]:gap-y-2 p-0!">
-                <div className="bg-green-950/50">
+                <div className="bg-green-950/40">
                   <p className="text-green-400 text-lg font-medium tracking-wide">24H High</p>
-                  <p className="text-lg font-medium">{`${currency.symbol} ${coinData.market_data.high_24h[currency.name]}`}</p>
+                  <p className="text-xl font-medium tracking-wide">{`${currency.symbol} ${coinData.market_data.high_24h[currency.name]}`}</p>
                 </div>
-                <div className="bg-red-950/40">
+                <div className="bg-red-950/30">
                   <p className="text-red-400 text-lg font-medium tracking-wide">24H Low</p>
-                  <p className="text-lg font-medium">{`${currency.symbol} ${coinData.market_data.low_24h[currency.name]}`}</p>
+                  <p className="text-xl font-medium tracking-wide">{`${currency.symbol} ${coinData.market_data.low_24h[currency.name]}`}</p>
                 </div>
               </div>
 
               <div className="col-span-2 row-span-1 flex gap-4 !p-0 [&>*]:p-4 [&>*]:flex [&>*]:gap-x-3 [&>*]:items-center [&>*]:justify-center [&>*]:rounded-2xl">
-                <div className="bg-primary-100 w-full">
+                <div className="bg-primary-100/40 w-full">
                   <FaRegThumbsUp className="fill-green-400 size-5"/>
                   <p className="text-green-400 text-lg font-medium">{`${coinData.sentiment_votes_up_percentage} %`}</p>
                 </div>
-                <div className="bg-primary-100 w-full">
+                <div className="bg-primary-100/40 w-full">
                   <FaRegThumbsDown className="fill-red-400 size-5"/>
                   <p className="text-red-400 text-lg font-medium">{`${coinData.sentiment_votes_down_percentage} %`}</p>
                 </div>                
               </div>
 
               <div className="col-span-2 row-span-1 flex gap-x-4 p-0! [&>*]:rounded-2xl [&>*]:p-4 [&>*]:flex [&>*]:items-center [&>*]:justify-center">
-                <a href={coinData.links.subreddit_url} target="_blank" className={`${coinData.links.subreddit_url ? "cursor-pointer" : "cursor-not-allowed"} bg-primary-100 w-full`} rel="noopener noreferrer">
-                  <FaReddit className="size-10"/>
-                </a>
-                <a href={coinData.links.repos_url.github[0]} target="_blank" rel="noopener noreferrer"
-                className={`${coinData.links.repos_url.github.length > 0 ? "cursor-pointer" : "cursor-not-allowed"} bg-primary-100 w-full`}>
-                 <FaGithub className="size-10"/> 
-                </a>
+
+                {
+                  coinData.links.subreddit_url ? (
+                    <a href={coinData.links.subreddit_url} target="_blank" className="cursor-pointer bg-primary-100/40 hover:bg-primary-100/80 w-full relative" rel="noopener noreferrer">
+                      <BiLinkExternal className="size-6 stroke-accent-50 absolute top-2 right-2" strokeWidth= {0.5}/>
+                      <FaReddit className="size-10"/>
+                    </a>
+                  ) : (
+                    <div className="cursor-not-allowed bg-primary-100/40 text-primary-50/40 w-1/2">
+                      <p className="text-lg font-medium">N/A</p>
+                    </div>
+                  )
+                }
+
+                {
+                  coinData.links.repos_url.github.length > 0 ? (
+                    <a href={coinData.links.repos_url.github[0]} target="_blank" rel="noopener noreferrer"
+                className={`${coinData.links.repos_url.github.length > 0 ? "cursor-pointer" : "cursor-not-allowed"} bg-primary-100/40 hover:bg-primary-100/80 w-full relative`}>
+                      <BiLinkExternal className="size-6 stroke-accent-50 absolute top-2 right-2" strokeWidth= {0.5}/>
+                      <FaGithub className="size-10"/> 
+                    </a> 
+                  ) :
+                  
+                  (
+                    <div className="cursor-not-allowed bg-primary-100/40 text-primary-50/40 w-1/2">
+                      <p className="text-lg font-medium">N/A</p>
+                    </div>
+                  )
+                }
+                               
               </div>
 
-              <div className={`${coinData.links.whitepaper != "" ? "cursor-pointer" : "cursor-not-allowed opacity-40"} col-span-2 row-span-1 flex justify-center items-center bg-primary-100`}>
+              <div className={`${coinData.links.whitepaper != "" ? "cursor-pointer hover:bg-primary-100/80" : "cursor-not-allowed text-primary-50/40"} col-span-2 row-span-1 flex justify-center items-center bg-primary-100/40 relative`}>
                 {coinData.links.whitepaper ?
-                (<a href={coinData.links.whitepaper} target="_blank" rel="noopener noreferrer" className="flex items-center gap-x-2">
-                    <p className="text-lg font-medium">Whitepaper (PDF)</p>
-                    <RxExternalLink className="size-6 stroke-accent-50" strokeWidth= {0.5} />
-                  </a>) :
+                (<>
+                <BiLinkExternal className="size-6 stroke-accent-50 absolute top-2 right-2" strokeWidth= {0.5}/>
+                <a href={coinData.links.whitepaper} target="_blank" rel="noopener noreferrer" className="flex items-center gap-x-2">
+                    <p className="text-lg font-medium">Whitepaper (PDF)</p>                                        
+                  </a>
+                  </>) :
                   
                   (
                   <div className="flex items-center">
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -3,28 +3,92 @@ import Chart from 'react-google-charts';
 
 const LineChart = ( {historicalData} ) => {
 
-  const chartData = useMemo(
+    const chartData = useMemo(
     ()=>{
       if(!historicalData?.prices) return null;
 
       return [["Date", "Price"],
       ...historicalData.prices.map(
         (item)=> {
-          return [new Date(item[0]).toLocaleDateString("en-US", {month: "short", day: "numeric"}), item[1]]
+          return [new Date(item[0]), item[1]]
         }
       )]
     },
     [historicalData]
   );
 
+  const ticks = useMemo(
+    ()=>{
+      if(!historicalData?.prices) return undefined;
+
+      return Array.from(
+        new Set(
+          historicalData.prices.map(
+            item => new Date(item[0]).toDateString()
+          )
+        )
+      ).map(date => new Date(date))
+    },
+    [historicalData]
+  );
+
+  const options = {
+    backgroundColor : "transparent",    
+    chartArea: {
+      width: "90%",
+      height: "78%",
+      left: 65,
+      top: 30,
+    },
+    colors: ["#8b5cf6"],
+    lineWidth: 1.5,
+    curveType: "function",
+    legend:{
+      position: "none",
+    },
+    focusTarget: "category",
+    pointSize: 0,
+    tooltip: {
+    textStyle: {
+      color: "#060614",
+      fontSize: 13,
+    },   
+  },
+    hAxis: {
+      ticks,
+      format: "MMM d",
+      textStyle: {
+        color: "#f9f9fa",
+        fontSize: 12,
+      },
+      gridlines: {
+        color: "none",
+      },
+      
+    },
+    vAxis: {
+      textStyle: {
+        color: "#f9f9fa",
+      },
+      gridlines:{
+        color: "#191933",
+      }
+    },
+  };
+
   return (
     <div className="rounded-2xl overflow-hidden">
-      <Chart
-    chartType = "LineChart"
-    data = {chartData}
-    height = "300px"
-    legendToggle
-    />
+      {chartData && (
+        <Chart
+          key={chartData.length}
+          chartType = "LineChart"
+          data = {chartData}
+          options = {options}
+          height = "300px"
+    
+        />
+      )}
+      
     </div>
     
   )
